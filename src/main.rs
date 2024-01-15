f27f44e71d847029bafe52b74c1c7169a984e8f3
Fabrice Servais <fabrice.servais@gmail.com>
2024-01-15 23:57:18 +0100

    refacto creation of maps + test

--- src/main.rs
+++ src/main.rs
@@ -23,19 +23,13 @@ fn caesar_cipher(input: &str, shift: i32) -> String {
 
     let alphabet = "abcdefghijklmnopqrstuvwxyz".to_string();
 
-    let mut alphabet_index_to_char = HashMap::new();
-    let mut alphabet_char_to_index = HashMap::new();
-
     // Check that shift is positive
     if shift < 0 {
         return caesar_cipher(input, shift + alphabet.chars().count() as i32);
     }
 
-    for (i, c) in alphabet.chars().enumerate() {
-        alphabet_index_to_char.insert(i, c);
-        alphabet_char_to_index.insert(c, i);
-    }
-
+    // Create maps from alphabet string
+    let (alphabet_index_to_char, alphabet_char_to_index) = create_alphabet_maps(&alphabet);
 
     for c in input.chars() {
         let i = alphabet_char_to_index.get(&c).unwrap();
@@ -46,6 +40,21 @@ fn caesar_cipher(input: &str, shift: i32) -> String {
 
     output
 }
+
+// Creates 2 maps from an alphabet string
+fn create_alphabet_maps(alphabet: &str) -> (HashMap<usize, char>, HashMap<char, usize>) {
+    let mut alphabet_index_to_char = HashMap::new();
+    let mut alphabet_char_to_index = HashMap::new();
+
+    for (i, c) in alphabet.chars().enumerate() {
+        alphabet_index_to_char.insert(i, c);
+        alphabet_char_to_index.insert(c, i);
+    }
+
+    (alphabet_index_to_char, alphabet_char_to_index)
+}
+
+
 
 #[cfg(test)]
 mod tests {
@@ -58,5 +67,16 @@ mod tests {
         assert_eq!(caesar_cipher("hello", 26), "hello");
         assert_eq!(caesar_cipher("hello", 27), "ifmmp");
         assert_eq!(caesar_cipher("hello", -1), "gdkkn");
+    }
+
+    #[test]
+    fn test_create_alphabet_maps() {
+        let alphabet = "abcdefghijklmnopqrstuvwxyz";
+        let (index_to_char, char_to_index) = create_alphabet_maps(alphabet);
+
+        for (i, c) in alphabet.chars().enumerate() {
+            assert_eq!(index_to_char.get(&i), Some(&c));
+            assert_eq!(char_to_index.get(&c), Some(&i));
+        }
     }
 }
